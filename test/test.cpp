1152819f54d269dcabff9e337de0df0924ff7cb8
eseow <ethanseow00@gmail.com>
2023-08-25 16:11:57 -0400

    passed function expr test

--- test/test.cpp
+++ test/test.cpp
@@ -56,7 +56,7 @@ void testcase(std::string TEST_SUITE, std::string fileName)
     }
     else if (TEST_SUITE == PARSER_TEST_SUITE)
     {
-        // testcase(SCANNER_TEST_SUITE, fileName);
+        testcase(SCANNER_TEST_SUITE, fileName);
         NiceMock<ParserMock> parser(&file);
         parser.parse();
         actualOutput = parser.getExprsString();
